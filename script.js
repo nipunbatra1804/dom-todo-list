700fd10c9c18e31f99219e289e473248af1b864e
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-02-13 16:24:19 +0800

    refactored code

--- script.js
+++ script.js
@@ -1,71 +1,70 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
 
 function debugPrint(content) {
-	console.log(content);
+  console.log(content);
 }
 
-function removeButtonListener(event){
-	let btn = event.target;
-	const taskInfo = btn.previousSibling;
-	const divElement = btn.parentNode;
-	divElement.remove();
+function removeButtonListener(event) {
+  let btn = event.target;
+  const divElement = btn.parentNode;
+  divElement.remove();
 }
 
+function appendRemoveButton(listElement) {
+  const removeButton = document.createElement("button");
+  const parentNode = listElement.parentNode;
 
-function appendRemoveButton(listElement){
-	const removeButton = document.createElement('button');
-	const parentNode = listElement.parentNode;
-
-	removeButton.textContent = "remove";
-	removeButton.classList.add("remove");
-	removeButton.addEventListener('click',removeButtonListener);
-	parentNode.insertBefore(removeButton,listElement.nextSibling);
+  removeButton.textContent = "remove";
+  removeButton.classList.add("remove");
+  removeButton.addEventListener("click", removeButtonListener);
+  parentNode.insertBefore(removeButton, listElement.nextSibling);
 }
 
-function addArrayToList(array){
-	array.forEach(element => {
-		const li = document.createElement('li')
-		const div = document.createElement('div');
-		li.textContent = element;
-		
-		const ol = document.getElementById("todo-list");
-		div.appendChild(li);
-		appendRemoveButton(li);
-		div.classList.add("list-element");
-		ol.appendChild(div);
-	});
+function addTaskAsDiv(todoVal) {
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  li.textContent = todoVal;
+
+  const ol = document.getElementById("todo-list");
+  div.appendChild(li);
+  appendRemoveButton(li);
+  div.classList.add("list-element");
+  ol.appendChild(div);
 }
 
-function addItemToList(){
-	const inputBox = document.getElementById("task-name");
-	let inputText = inputBox.value;
-	const div = document.createElement('div');
-	const li = document.createElement('li')
-	li.textContent = inputText;
-	
-	const ol = document.getElementById("todo-list");
-	div.appendChild(li);
-	appendRemoveButton(li);
-	div.classList.add("list-element");
-	ol.appendChild(div);
-	
+function addArrayToList(array) {
+  array.forEach(element => {
+    addTaskAsDiv(element);
+  });
 }
 
-const buttonElement = document.querySelector('.add-task');
-buttonElement.addEventListener('click', ()=>{
-	addItemToList();
+function addItemToList() {
+  const inputBox = document.getElementById("task-name");
+  let inputText = inputBox.value;
+  addTaskAsDiv(inputText);
+  inputBox.value = "";
+}
+
+const buttonElement = document.querySelector(".add-task");
+buttonElement.addEventListener("click", () => {
+  addItemToList();
 });
 
-let listObject = document.getElementById("todo-list");
-listObject.addEventListener("click", function (event){
-	debugPrint(event.target);
-	let li =  event.target;
-	li.classList.toggle("done");
+const listObject = document.getElementById("todo-list");
+listObject.addEventListener("click", function(event) {
+  if (event.target.nodeName === "LI") {
+    let li = event.target;
+    li.classList.toggle("done");
+  }
 });
 
-addArrayToList(tasks)
+const inputBox = document.getElementById("task-name");
+inputBox.addEventListener("keydown", event => {
+  if (event.keyCode === 13) {
+    addTaskAsDiv(inputBox.value);
+    inputBox.value = "";
+  }
+  return;
+});
+
+addArrayToList(tasks);
